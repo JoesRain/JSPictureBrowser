81d7ea709a13db3e40ed968cd49d9dcc24a4f47c
BreazeMago <505554859@qq.com>
2017-04-23 21:29:20 +0800

    建立初始化项目

---
+++ BrowerDemo/BrowerDemo/JSPictureBrowser/JSPictureBrowserController.m
@@ -0,0 +1,23 @@
+//
+//  JSPictureBrowserController.m
+//  BrowerDemo
+//
+//  Created by BreazeMago on 2017/4/23.
+//  Copyright © 2017年 JoesRain. All rights reserved.
+//
+
+#import "JSPictureBrowserController.h"
+
+@interface JSPictureBrowserController ()
+
+@end
+
+@implementation JSPictureBrowserController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
